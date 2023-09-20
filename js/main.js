5c58dc922498b68065246aa3dd674c82f2939976
Thibault Helle <thibaulthelle@me.com>
2023-09-20 23:15:25 +0200

    updated content

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Alex Smith.", "Designer.", "Developer.", "Freelancer.", "Photographer"],
+      strings: ["Thibault Helle.", "An Entrepreneur.", "An Engineer.", "A Developer."],
       typeSpeed: 100,
       loop: true,
     });
